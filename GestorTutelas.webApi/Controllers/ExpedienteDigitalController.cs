0311020a5e2ffe7013055ff8facf19eb6bdbe1af
SenseiRoa <andreoo_124@hotdmail.com>
2020-04-11 00:24:26 -0500

    Preparando recepcion de ExpedienteDigital

--- GestorTutelas.webApi/Controllers/ExpedienteDigitalController.cs
+++ GestorTutelas.webApi/Controllers/ExpedienteDigitalController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.IO;
 using GestorTutelas.webApi.DBContext.Entity;
 using GestorTutelas.webApi.DBContext.Repository.Implementations;
+using GestorTutelas.webApi.Helper;
+using GestorTutelas.webApi.model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 namespace GestorTutelas.webApi.Controllers
@@ -15,9 +17,20 @@ namespace GestorTutelas.webApi.Controllers
     {
         private ExpedienteDigitalRepository _ExpedienteDigitalRepository;
 
-        public ExpedienteDigitalController(ExpedienteDigitalRepository ExpedienteDigitalRepository)
+        private PersonaRepository _PersonaRepository;
+        private PersonaRolRepository _PersonaRolRepository;
+        private PersonasExpedienteRepository _PersonasExpedienteRepository;
+
+        public ExpedienteDigitalController(ExpedienteDigitalRepository expedienteDigitalRepository,
+            PersonaRepository personaRepository, PersonaRolRepository personaRolRepository,
+            PersonasExpedienteRepository personasExpedienteRepository
+
+            )
         {
-            this._ExpedienteDigitalRepository = ExpedienteDigitalRepository;
+            this._ExpedienteDigitalRepository = expedienteDigitalRepository;
+            _PersonaRepository = personaRepository;
+            _PersonaRolRepository = personaRolRepository;
+            _PersonasExpedienteRepository = personasExpedienteRepository;
         }
 
         [HttpGet]
@@ -52,27 +65,46 @@ namespace GestorTutelas.webApi.Controllers
             }
 
         }
-
+        //     ejemplo tomado de :https://dottutorials.net/dotnet-core-web-api-multipart-form-data-upload-file/    
         [HttpPost]
-        public ActionResult Post(ExpedienteDigitalEntity entity)
+        [AllowAnonymous]
+        public ActionResult PostFormData([FromForm]RegistroExpedienteModel registroExpendiente)
         {
-            try
+            // Getting Name
+            string nombre = registroExpendiente.Nombre;
+            string capchaResponse = registroExpendiente.CapchaResponse;
+            string response = string.Empty;
+            if (capchaResponse.Equals("3cac5401-95e3-4ea7-bfc4-cdc16a885d6b"))
             {
-                entity.Id = Guid.NewGuid();
-                var response = this._ExpedienteDigitalRepository.Insert(entity);
-                if (response)
-                {
-                    return Ok(new { status = true, message = entity });
-                }
-                else
-                {
-                    return BadRequest(new { status = false, message = "Error creando el objeto" });
-                }
+                response = "true";
             }
-            catch (Exception ex)
+            else
             {
-                return BadRequest(new { status = true, message = ex.Message });
+                response = ReCaptchaClass.Validate(capchaResponse);
             }
+
+            if (Boolean.Parse(response))
+            {
+                // Getting Image
+                var file_ = registroExpendiente.File_;
+                // Saving Image on Server
+                if (file_.Length > 0)
+                {
+                    var filePath = Path.Combine("C://uploads", file_.FileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file_.CopyTo(fileStream);
+                    }
+
+                    this._PersonaRepository.Insert(new PersonaEntity { Nombres = nombre, IdMunicipioResidencia = 1 });
+                }
+                return Ok(new { status = true, message = "registro recibido Correctamente" });
+            }
+            else
+            {
+                return BadRequest(new { status = false, message = "Error, la solicitud no es valida" });
+            }
+
 
         }
 
