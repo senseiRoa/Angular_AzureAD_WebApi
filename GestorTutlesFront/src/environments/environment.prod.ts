6bff907a5166b68c70c344bc4c8a8d2c9e4381e5
SenseiRoa <andreoo_124@hotdmail.com>
2020-04-23 12:32:17 -0500

    Actualizacion docker compose and docker file

--- GestorTutlesFront/src/environments/environment.prod.ts
+++ GestorTutlesFront/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+
+  redirectUrl: 'http://accionesvirtuales.cortesuprema.gov.co/Recursos',
+  //redirectUrl: 'http://localhost:4200/Recursos',
+  clientId: 'd3127a6a-136f-4f9e-946c-c6151ed297f3',
+  clientIdAPI: 'api://0ddb5b33-3b13-4402-b0e0-048bd1028ed1/api-access',
+  authority: 'https://login.microsoftonline.com/622cba98-80f8-41f3-8df5-8eb99901598b',
+
+  aadUserReadScope: 'user.read',
+  privateApiUrl: 'http://expedientedigitalservice.cortesuprema.gov.co/api/',
+  publicApiUrl: 'http://expedientedigitalservice.cortesuprema.gov.co/pubapi/',
+
+
 };
